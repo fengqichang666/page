2104c87202ceea71663c4b6bd2f9dbf869fd4ebe
冯琦昌 <2309997549@qq.com>
2022-08-25 21:33:51 +0800

    ToDoList

--- index.js
+++ index.js
@@ -2,16 +2,26 @@
  * @Author: 冯琦昌 2309997549@qq.com
  * @Date: 2022-08-20 14:26:08
  * @LastEditors: 冯琦昌 2309997549@qq.com
- * @LastEditTime: 2022-08-22 21:44:06
+ * @LastEditTime: 2022-08-25 21:33:00
  * @FilePath: \page\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+$("#search").click(function () {
+    let url = $(".dropdown-menu1 .active").attr("data-url")
+    window.open(url + $('#searchValue').val())
+})
+$('#searchValue').keypress(function (even) {
+    if (13 === even.which) {
+        $("#search").click()
+    }
+});
 $('.dropdown-menu1 li').click(function () {
     $('#dropdownMenuButton1').text($(this).text())
     $(this).siblings().find('a').removeClass('active')
     $(this).find('a').addClass('active')
 })
 let okrList = []
+let toDoList = []
 function creatCard() {
     localStorage.getItem("OKRLIST") && JSON.parse(localStorage.getItem("OKRLIST")).length > 0 ? okrList = JSON.parse(localStorage.getItem("OKRLIST")) : addCard();
     okrList.length === 0?.return;
@@ -26,6 +36,19 @@ function creatCard() {
     })
 }
 creatCard()
+function creatToDoList() {
+    localStorage.getItem("TODOLIST") && JSON.parse(localStorage.getItem("TODOLIST")).length > 0 ? toDoList = JSON.parse(localStorage.getItem("TODOLIST")) : addLine();
+    toDoList.length === 0?.return;
+    $(toDoList).each(function (i, item) {
+        addLine()
+        $("#todolist-container .todolist-input").eq(i).val(toDoList[i].content).hide()
+        $("#todolist-container .show-todo").eq(i).text(toDoList[i].content).show()
+        $("#todolist-container .time").eq(i).text(toDoList[i].time).show()
+        $("#todolist-container .saveLine").eq(i).hide()
+        $("#todolist-container .editLine").eq(i).show()
+    })
+}
+creatToDoList()
 $('.okr-add-card-body').click(function () {
     addCard()
 })
@@ -73,7 +96,7 @@ $('#deleteOkr').click(function () {
     $('#back').show()
 })
 // 删除OKR卡片
-$('main').on("click",".remove-Card .bi-trash",function () {
+$('main').on("click", ".remove-Card .bi-trash", function () {
     $(this).parents('.card').remove()
     setOkrItem()
 })
@@ -99,11 +122,81 @@ function removeOkrItem() {
     $("main .okr-card").remove()
     localStorage.removeItem('OKRLIST')
 }
+$("#addLine").click(function () {
+    addLine()
+})
+function resetLineNumber() {
+    $("#todolist-container .line-num").each(function (i) {
+        $(this).text(i + 1)
+    })
+}
+function addLine() {
+    $('#todolist-container').append($('#templateToDoList').html())
+    resetLineNumber()
+}
+$('#todolist-container').on("click", ".saveLine", function () {
+    $(this).hide().siblings("input").hide().siblings(".time, .editLine, .delLine").show().siblings(".cancel").hide()
+    $(this).siblings(".show-todo").text($(this).siblings("input").val()).show()
+    let time = getFormatDate()
+    $(this).siblings(".time").text(time)
+    let index = $("#todolist-container .saveLine").index($(this))
+    setToDoListItem(index)
+})
+$('#todolist-container').on("click", ".editLine", function () {
+    $(this).hide().siblings("input").show().siblings(".delLine, .show-todo, .time").hide()
+    $(this).siblings(".saveLine").show()
+    $(this).siblings(".cancel").show()
+})
+$('#todolist-container').on("click", ".cancel", function () {
+    $(this).hide().siblings("input").hide().siblings(".time, .editLine, .show-todo, .delLine").show()
+    let index = $("#todolist-container .cancel").index($(this))
+    $(this).siblings('input').val(toDoList[index].content)
+    $(this).siblings(".saveLine").hide()
+})
+$('#todolist-container').on("click", ".delLine", function () {
+    $(this).parents('.line').remove()
+    let index = $("#todolist-container .delLine").index($(this))
+    delLineItem(index)
+    resetLineNumber()
+})
+function getFormatDate() {
+    var nowDate = new Date();
+    var year = nowDate.getFullYear();
+    var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1;
+    var date = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
+    var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+    var minute = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+    var second = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+    return year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
+}
+function delLineItem(index){
+    toDoList.splice(index,1)
+    localStorage.setItem("TODOLIST", JSON.stringify(toDoList))
+}
+function setToDoListItem(index) {
+    toDoList[index] = {
+        content: $(".todolist-input").eq(index).val(),
+        time: $("#todolist-container .time").eq(index).text()
+    }
+    localStorage.setItem("TODOLIST", JSON.stringify(toDoList))
+}
+$("#clearToDoList").click(function () {
+    $("#todolist-container").empty()
+    clearToDoList()
+})
+function clearToDoList(){
+    let res = confirm("确定清空所有TODOLIST？")
+    if (res) {
+        localStorage.removeItem('TODOLIST')
+        clearToDoList()
+    }
+}
 //清空所有
 $("#clear").click(function () {
-    let res = confirm("确定所有数据？")
+    let res = confirm("确定删除所有数据？")
     if (res) {
         localStorage.removeItem('OKRLIST')
+        clearToDoList()
     }
     alert("删除成功！")
     window.location.reload()
